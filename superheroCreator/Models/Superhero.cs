baf3de7544059e96352cc48e8644d2e015fea3d5
Adam Neujahr <adamneujahr@gmail.com>
2019-10-24 14:05:58 -0500

    Created Superhero model.

--- superheroCreator/Models/Superhero.cs
+++ superheroCreator/Models/Superhero.cs
@@ -9,5 +9,9 @@ namespace superheroCreator.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public string AlterEgo { get; set; }
+        public string PrimaryAbility { get; set; }
+        public string SecondaryAbility { get; set; }
+        public string Catchphrase { get; set; }
     }
 }
