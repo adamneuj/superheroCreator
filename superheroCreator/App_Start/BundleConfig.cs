267b12ddba52091faead39ef1b58d699b6bab9e5
Adam Neujahr <adamneujahr@gmail.com>
2019-10-25 14:02:20 -0500

    Added bootstrap theme

--- superheroCreator/App_Start/BundleConfig.cs
+++ superheroCreator/App_Start/BundleConfig.cs
@@ -23,7 +23,7 @@ namespace superheroCreator
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap1.css",
                       "~/Content/site.css"));
         }
     }
